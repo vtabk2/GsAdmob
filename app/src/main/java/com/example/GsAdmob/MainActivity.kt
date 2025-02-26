c6ef99e5c5aabe5b526ea536795d83f9928bb9f3
Tuấn Anh <vtabk2@gmail.com>
2025-02-26 09:49:56 +0700

    admob

--- app/src/main/java/com/example/GsAdmob/MainActivity.kt
+++ app/src/main/java/com/example/GsAdmob/MainActivity.kt
@@ -25,41 +25,58 @@ class MainActivity : AppCompatActivity() {
 //        bindingView.nativeTest1.applyBuilder(builder)
 
         bindingView.nativeTest1.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.ALBUM
+        })
+
+        bindingView.nativeTest2.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.FONT
+        })
+
+        bindingView.nativeTest3.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.FRAME
+        })
+
+        bindingView.nativeTest4.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.LANGUAGE
+        })
+
+        bindingView.nativeTest5.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.SHARE
+        })
+
+        bindingView.nativeTest6.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.STICKER
+        })
+
+        bindingView.nativeTest7.applyBuilder(BaseNativeAdView.Builder().apply {
+            adsMode = AdsMode.TEMPLATE
+        })
+
+        bindingView.nativeTest8.applyBuilder(BaseNativeAdView.Builder().apply {
             adsMode = AdsMode.VIP
         })
 
-//        bindingView.nativeTest1.gone()
-//        bindingView.nativeAlbum.gone()
-//        bindingView.nativeFont.gone()
-//        bindingView.nativeFrame1.gone()
-//        bindingView.nativeFrame2.gone()
-//        bindingView.nativeLanguage.gone()
-//        bindingView.nativeShare.gone()
-//        bindingView.nativeSticker.gone()
-//        bindingView.nativeTemplate.gone()
-//        bindingView.nativeCustom.gone()
-
         NativeUtils.loadNativeAds(this, this, isVip = false, callbackStart = {
             bindingView.nativeTest1.startShimmer()
-//            bindingView.nativeAlbum.startShimmer()
-//            bindingView.nativeFont.startShimmer()
-//            bindingView.nativeFrame1.startShimmer()
-//            bindingView.nativeFrame2.startShimmer()
-//            bindingView.nativeLanguage.startShimmer()
-//            bindingView.nativeShare.startShimmer()
-//            bindingView.nativeSticker.startShimmer()
-//            bindingView.nativeTemplate.startShimmer()
+            bindingView.nativeTest2.startShimmer()
+            bindingView.nativeTest3.startShimmer()
+            bindingView.nativeTest4.startShimmer()
+            bindingView.nativeTest5.startShimmer()
+            bindingView.nativeTest6.startShimmer()
+            bindingView.nativeTest7.startShimmer()
+            bindingView.nativeTest8.startShimmer()
+
             bindingView.nativeCustom.startShimmer()
         }, callback = { nativeAd ->
             bindingView.nativeTest1.setNativeAd(nativeAd)
-//            bindingView.nativeAlbum.setNativeAd(nativeAd)
-//            bindingView.nativeFont.setNativeAd(nativeAd)
-//            bindingView.nativeFrame1.setNativeAd(nativeAd)
-//            bindingView.nativeFrame2.setNativeAd(nativeAd)
-//            bindingView.nativeLanguage.setNativeAd(nativeAd)
-//            bindingView.nativeShare.setNativeAd(nativeAd)
-//            bindingView.nativeSticker.setNativeAd(nativeAd)
-//            bindingView.nativeTemplate.setNativeAd(nativeAd)
+            bindingView.nativeTest2.setNativeAd(nativeAd)
+            bindingView.nativeTest3.setNativeAd(nativeAd)
+            bindingView.nativeTest4.setNativeAd(nativeAd)
+            bindingView.nativeTest5.setNativeAd(nativeAd)
+            bindingView.nativeTest6.setNativeAd(nativeAd)
+            bindingView.nativeTest7.setNativeAd(nativeAd)
+            bindingView.nativeTest8.setNativeAd(nativeAd)
+
             bindingView.nativeCustom.setNativeAd(nativeAd)
         })
     }

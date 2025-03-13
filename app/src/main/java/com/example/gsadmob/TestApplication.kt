0df57221f4cde70282da9b040e34a74be896d5d2
Tuấn Anh <vtabk2@gmail.com>
2025-03-13 11:37:43 +0700

    delete AppOpenAdManager

--- app/src/main/java/com/example/gsadmob/TestApplication.kt
+++ app/src/main/java/com/example/gsadmob/TestApplication.kt
@@ -1,18 +1,11 @@
 package com.example.gsadmob
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import com.core.gsadmob.appopen.AppOpenAdManager
-import com.core.gsadmob.appopen.AppResumeAdManager
-import com.core.gsadmob.callback.AdGsListener
 import com.core.gsadmob.utils.AdGsManager
 import com.gs.core.GsApplication
 import kotlinx.coroutines.MainScope
 
 class TestApplication : GsApplication() {
-    private var appOpenAdManager: AppOpenAdManager? = null
-    private var appResumeAdManager: AppResumeAdManager? = null
-
     init {
         instance = this
     }
@@ -27,30 +20,6 @@ class TestApplication : GsApplication() {
 
     override fun initConfig() {
         AdGsManager.instance.registerCoroutineScope(application = this, coroutineScope = MainScope())
-    }
-
-    fun initOpenAds() {
-        if (appOpenAdManager == null) {
-            appOpenAdManager = AppOpenAdManager().apply {
-                loadAd(this@TestApplication)
-            }
-        } else {
-            if (appOpenAdManager?.checkHasAds() == false) {
-                appOpenAdManager?.loadAd(this)
-            }
-        }
-    }
-
-    fun setAdOpenListener(listener: AdGsListener?) {
-        appOpenAdManager?.listener = listener
-    }
-
-    fun checkHasAds(): Boolean {
-        return appOpenAdManager?.checkHasAds() ?: false
-    }
-
-    fun showAdIfAvailable(activity: Activity, isVip: Boolean, callbackSuccess: () -> Unit, callbackFailed: () -> Unit) {
-        appOpenAdManager?.showAdIfAvailable(activity, isVip, callbackSuccess, callbackFailed)
     }
 
     companion object {

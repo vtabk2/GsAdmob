00796d3e8763aae6fd1ad534e5ef61d3fdc4e0ac
Tuấn Anh <vtabk2@gmail.com>
2025-03-15 20:19:57 +0700

    banner

--- GsAdmob/src/main/java/com/core/gsadmob/banner/BannerGsAdView.kt
+++ GsAdmob/src/main/java/com/core/gsadmob/banner/BannerGsAdView.kt
@@ -1,164 +1,118 @@
 package com.core.gsadmob.banner
 
-import android.app.Activity
 import android.content.Context
-import android.os.Build
-import android.os.Bundle
-import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.WindowMetrics
 import android.widget.FrameLayout
+import androidx.core.content.withStyledAttributes
 import com.core.gsadmob.R
-import com.core.gsadmob.databinding.AdBannerShimmerBinding
+import com.core.gsadmob.databinding.AdBannerGsAdViewBinding
 import com.core.gscore.utils.extensions.gone
 import com.core.gscore.utils.extensions.invisible
 import com.core.gscore.utils.extensions.visible
-import com.facebook.shimmer.ShimmerFrameLayout
-import com.google.ads.mediation.admob.AdMobAdapter
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
+import com.core.gscore.utils.extensions.visibleIf
 import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.LoadAdError
 
-class BannerGsAdView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
-    private var bannerAds: AdView? = null
-    private var shimmerView: ShimmerFrameLayout? = null
+class BannerGsAdView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+    private var binding = AdBannerGsAdViewBinding.inflate(LayoutInflater.from(context), this, true)
+    private var bannerView: AdView? = null
 
-    private var delayTime = 0L
-    private var lastTime = 0L
+    private var adsAlwaysShow: Boolean = false
 
-    // delay thời gian load lại banner tính bằng giây
-    fun registerDelayTime(time: Long) {
-        delayTime = time
-    }
+    var adsShow: Boolean = false
+        set(value) {
+            field = value
 
-    init {
-        bannerAds = AdView(context)
-        bannerAds?.descendantFocusability = ViewGroup.FOCUS_BLOCK_DESCENDANTS
-        val params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-        params.gravity = Gravity.BOTTOM
-        addView(bannerAds, params)
-
-        val adSize = getAdSize(context)
-        bannerAds?.setAdSize(adSize)
-
-        val binding = AdBannerShimmerBinding.inflate(LayoutInflater.from(context))
-        shimmerView = binding.adShimmerBanner
-        shimmerView?.let {
-            addView(shimmerView)
-        }
-    }
-
-    fun loadAds(
-        isVip: Boolean, // true: vip, false: free -> sẽ hiển thị ads
-        adUnitId: Int = R.string.banner_id, // truyền id banner
-        show: Boolean = true, // true: show banner, false: không show banner
-        alwaysShow: Boolean = false, // tác dụng để banner ko gone()
-        isCollapsible: Boolean = false, // nếu muốn dùng collapsible banner
-        callbackShow: (() -> Unit)? = null, // callback khi show banner thành công
-    ) {
-        if (isVip) {
-            stopShimmer()
-            if (!alwaysShow) {
-                gone()
-            }
-            return
-        }
-
-        if (delayTime > 0) {
-            val currentTime = System.currentTimeMillis()
-            if (currentTime - lastTime < delayTime * 1000) return
-            lastTime = currentTime
-        }
-
-        startShimmer()
-
-        if (TextUtils.isEmpty(bannerAds?.adUnitId)) {
-            bannerAds?.adUnitId = context.getString(adUnitId)
-        }
-        // Create an extra parameter that aligns the bottom of the expanded ad to
-        // the bottom of the bannerView.
-        val extras = Bundle()
-        if (isCollapsible) {
-            extras.putString("collapsible", "bottom")
-        }
-        val adRequest = AdRequest.Builder().addNetworkExtrasBundle(AdMobAdapter::class.java, extras).setHttpTimeoutMillis(5000).build()
-        bannerAds?.loadAd(adRequest)
-
-        bannerAds?.adListener = object : AdListener() {
-            override fun onAdLoaded() {
-                stopShimmer()
-                if (show) {
-                    bannerAds?.visible()
-                    callbackShow?.invoke()
-                } else {
-                    if (alwaysShow) {
-                        bannerAds?.visible()
-                        callbackShow?.invoke()
-                    } else {
-                        gone()
-                    }
-                }
-                super.onAdLoaded()
-            }
-
-            override fun onAdFailedToLoad(loadAdError: LoadAdError) {
-                super.onAdFailedToLoad(loadAdError)
-                stopShimmer()
-                if (alwaysShow) {
-                    bannerAds?.visible()
-                    callbackShow?.invoke()
+            if (bannerView == null) {
+                if (adsAlwaysShow) {
+                    invisible()
                 } else {
                     gone()
                 }
+                return
+            }
+
+            visibleIf(adsShow, adsAlwaysShow)
+        }
+
+    private var adsAutoStartShimmer: Boolean = true
+        set(value) {
+            field = value
+            if (field) {
+                startShimmer()
+            } else {
+                stopShimmer()
+            }
+        }
+
+    init {
+        attrs?.let {
+            context.withStyledAttributes(it, R.styleable.BannerGsAdView) {
+                adsAlwaysShow = getBoolean(R.styleable.BannerGsAdView_adsAlwaysShow, adsAlwaysShow)
+                adsShow = getBoolean(R.styleable.BannerGsAdView_adsShow, adsShow)
+                adsAutoStartShimmer = getBoolean(R.styleable.BannerGsAdView_adsAutoStartShimmer, adsAutoStartShimmer)
             }
         }
     }
 
-    private fun getAdSize(context: Context): AdSize {
-        val displayMetrics = context.resources.displayMetrics
-        val adWidthPixels = try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                val windowMetrics: WindowMetrics = (context as Activity).windowManager.currentWindowMetrics
-                windowMetrics.bounds.width()
-            } else {
-                displayMetrics.widthPixels
+    fun setBannerAdView(adView: AdView?) {
+        this.bannerView = adView
+
+        binding.apply {
+            adsBannerView.removeAllViews()
+            stopShimmer()
+
+            if (bannerView == null) {
+                if (adsAlwaysShow) {
+                    invisible()
+                } else {
+                    gone()
+                }
+                return
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            displayMetrics.widthPixels
+
+            visibleIf(adsShow, adsAlwaysShow)
+
+            val params = adsBannerView.layoutParams as LayoutParams
+            params.gravity = Gravity.BOTTOM
+
+            // Kiểm tra nếu view đã có parent
+            if (bannerView?.parent != null) {
+                (bannerView?.parent as? ViewGroup)?.removeView(bannerView) // Xóa view khỏi parent hiện tại
+            }
+            adsBannerView.addView(bannerView, params)
         }
-        val density = displayMetrics.density
-        val adWidth = (adWidthPixels / density).toInt()
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, adWidth)
     }
 
     fun startShimmer() {
+        if (!adsShow) return
         visible() // quan trọng, nếu không visible() thì sẽ ko hiển thị đc view
-        shimmerView?.visible()
-        shimmerView?.showShimmer(true)
-        bannerAds?.invisible()
+        binding.adsShimmerBanner.visible()
+        binding.adsShimmerBanner.showShimmer(true)
+        binding.adsBannerView.invisible()
     }
 
     fun stopShimmer() {
-        shimmerView?.hideShimmer()
-        shimmerView?.gone()
-        bannerAds?.visible()
-    }
-
-    fun resume() {
-        bannerAds?.resume()
+        binding.adsShimmerBanner.hideShimmer()
+        binding.adsShimmerBanner.gone()
+        binding.adsBannerView.visibleIf(adsShow, adsAlwaysShow)
     }
 
     fun pause() {
-        bannerAds?.pause()
+        bannerView?.pause()
+    }
+
+    fun resume() {
+        bannerView?.resume()
     }
 
     fun destroy() {
-        bannerAds?.destroy()
+        try {
+            bannerView?.destroy()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }

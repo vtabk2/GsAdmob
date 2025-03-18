b7406f3f3fd0a6d70f250980f05ffb56b4c40673
Tuấn Anh <vtabk2@gmail.com>
2025-03-18 14:03:01 +0700

    fix

--- app/src/main/java/com/example/gsadmob/ui/activity/TestNativeActivity.kt
+++ app/src/main/java/com/example/gsadmob/ui/activity/TestNativeActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.core.gsadmob.banner.BannerGsAdView
 import com.core.gsadmob.model.AdPlaceName
-import com.core.gsadmob.model.banner.BannerAdGsData
 import com.core.gsadmob.model.nativead.NativeAdGsData
 import com.core.gsadmob.utils.AdGsManager
 import com.core.gsadmob.utils.AdPlaceNameConfig
@@ -40,58 +39,6 @@ class TestNativeActivity : BaseAdsActivity<ActivityTestNativeBinding>() {
                         bindingView.tvActiveVip.text = "Vip Active"
                     } else {
                         bindingView.tvActiveVip.text = "Vip Inactive"
-                    }
-                }
-            }
-
-            async {
-                AdGsManager.instance.adGsDataMapMutableStateFlow.collect {
-                    it.forEach { adGsDataMap ->
-                        when (adGsDataMap.key) {
-                            AdPlaceNameConfig.AD_PLACE_NAME_BANNER -> {
-                                if (adGsDataMap.value.isLoading) {
-                                    bindingView.bannerView.startShimmer()
-                                } else {
-                                    bindingView.bannerView.setBannerAdView((adGsDataMap.value as? BannerAdGsData)?.bannerAdView)
-                                }
-                            }
-
-                            AdPlaceNameConfig.AD_PLACE_NAME_NATIVE -> {
-                                if (adGsDataMap.value.isLoading) {
-                                    bindingView.nativeFrame.startShimmer()
-                                } else {
-                                    bindingView.nativeFrame.setNativeAd((adGsDataMap.value as? NativeAdGsData)?.nativeAd)
-                                }
-                            }
-
-                            AdPlaceNameConfig.AD_PLACE_NAME_NATIVE_LANGUAGE -> {
-                                if (adGsDataMap.value.isLoading) {
-                                    bindingView.nativeLanguage.startShimmer()
-                                } else {
-                                    bindingView.nativeLanguage.setNativeAd((adGsDataMap.value as? NativeAdGsData)?.nativeAd)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        AdGsManager.instance.startShimmerLiveData.observe(this) { shimmerMap ->
-            shimmerMap.forEach {
-                if (it.value) {
-                    when (it.key) {
-                        AdPlaceNameConfig.AD_PLACE_NAME_BANNER -> {
-                            bindingView.bannerView.startShimmer()
-                        }
-
-                        AdPlaceNameConfig.AD_PLACE_NAME_NATIVE -> {
-                            bindingView.nativeFrame.startShimmer()
-                        }
-
-                        AdPlaceNameConfig.AD_PLACE_NAME_NATIVE_LANGUAGE -> {
-                            bindingView.nativeLanguage.startShimmer()
-                        }
                     }
                 }
             }

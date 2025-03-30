9c16e913e15caac26d9fb298abbc4ebad018081c
Tuấn Anh <vtabk2@gmail.com>
2025-03-30 22:57:38 +0700

    RemoteConfigUtils

---
+++ GsAdmob/src/main/java/com/core/gsadmob/utils/RemoteConfigUtils.kt
@@ -0,0 +1,53 @@
+package com.core.gsadmob.utils
+
+import android.app.Activity
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.remoteconfig.remoteConfig
+
+abstract class RemoteConfigUtils {
+    fun initRemoteConfig(activity: Activity, remoteConfigDefaultsId: Int) {
+        // [START get_remote_config_instance]
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        // [END get_remote_config_instance]
+
+        // [START enable_dev_mode]
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        // [END enable_dev_mode]
+
+        // [START set_default_values]
+        remoteConfig.setDefaultsAsync(remoteConfigDefaultsId)
+        // [END set_default_values]
+
+        // [START fetch_config_with_callback]
+        remoteConfig.fetchAndActivate().addOnCompleteListener(activity) { task ->
+            if (task.isSuccessful) {
+                updateRemoteConfig(remoteConfig, "fetchAndActivate")
+            } else {
+                // nothing
+            }
+        }
+        // [END fetch_config_with_callback]
+
+        // [START add_config_update_listener]
+        remoteConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) {
+                updateRemoteConfig(remoteConfig, "onUpdate")
+            }
+
+            override fun onError(error: FirebaseRemoteConfigException) {}
+        })
+    }
+
+    open fun updateRemoteConfig(remoteConfig: FirebaseRemoteConfig, from: String) {
+        Log.d("TAG5", "RemoteConfigUtils_updateRemoteConfig: from = $from")
+    }
+}

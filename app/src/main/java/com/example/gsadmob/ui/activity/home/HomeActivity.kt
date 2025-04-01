d9832f2bcbbd97b278f22ca0054b44aeb7951df3
Tuấn Anh <vtabk2@gmail.com>
2025-04-01 16:57:03 +0700

    language

--- app/src/main/java/com/example/gsadmob/ui/activity/home/HomeActivity.kt
+++ app/src/main/java/com/example/gsadmob/ui/activity/home/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gsadmob.ui.activity.home
 
+import android.content.Intent
 import com.core.gsadmob.banner.BannerGsAdView
 import com.core.gsadmob.model.AdPlaceName
 import com.core.gsadmob.model.nativead.NativeAdGsData
@@ -7,6 +8,7 @@ import com.core.gsadmob.utils.AdGsManager
 import com.example.gsadmob.AdGsRemoteExtraConfig
 import com.example.gsadmob.databinding.ActivityHomeBinding
 import com.example.gsadmob.ui.activity.base.BaseAdsActivity
+import com.example.gsadmob.ui.activity.language.LanguageActivity
 
 class HomeActivity : BaseAdsActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate) {
     override val bannerGsAdView: BannerGsAdView by lazy { bindingView.bannerView }
@@ -16,6 +18,14 @@ class HomeActivity : BaseAdsActivity<ActivityHomeBinding>(ActivityHomeBinding::i
             AdGsRemoteExtraConfig.instance.adPlaceNameBannerHome,
             AdGsRemoteExtraConfig.instance.adPlaceNameNativeHome
         )
+    }
+
+    override fun initListener() {
+        super.initListener()
+
+        bindingView.tvLanguage.setOnClickListener {
+            startActivity(Intent(this, LanguageActivity::class.java))
+        }
     }
 
     override fun setupNative(adPlaceName: AdPlaceName, nativeAdGsData: NativeAdGsData?) {

c624a933e60513c688a99a45a21a7f49a93cfa07
Tuấn Anh <vtabk2@gmail.com>
2025-04-01 16:45:55 +0700

    cấu hình BaseAdsActivity có thể sử dụng BANNER_COLLAPSIBLE

--- app/src/main/java/com/example/gsadmob/ui/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/example/gsadmob/ui/activity/splash/SplashActivity.kt
@@ -10,7 +10,7 @@ import com.example.gsadmob.AdGsRemoteExtraConfig
 import com.example.gsadmob.R
 import com.example.gsadmob.TestApplication
 import com.example.gsadmob.databinding.ActivitySplashBinding
-import com.example.gsadmob.ui.activity.TestAdsActivity
+import com.example.gsadmob.ui.activity.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
     private var bindingView: ActivitySplashBinding? = null
@@ -40,7 +40,7 @@ class SplashActivity : AppCompatActivity() {
 
     private fun goToHome() {
         if (isTaskRoot) {
-            val intent = Intent(this@SplashActivity, TestAdsActivity::class.java)
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
         }

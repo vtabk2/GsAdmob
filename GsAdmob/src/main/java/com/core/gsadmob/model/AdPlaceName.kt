a72c0d426039366c6829f5ca60f002a78493ebc3
Tuấn Anh <vtabk2@gmail.com>
2025-04-09 09:57:42 +0700

    thêm SerializedName AdPlaceName

--- GsAdmob/src/main/java/com/core/gsadmob/model/AdPlaceName.kt
+++ GsAdmob/src/main/java/com/core/gsadmob/model/AdPlaceName.kt
@@ -1,32 +1,35 @@
 package com.core.gsadmob.model
 
+import android.text.TextUtils
+import com.google.gson.annotations.SerializedName
+
 data class AdPlaceName(
     /**
      * Tên của quảng cáo = loại quảng cáo + tên màn hình(Ví dụ : banner_home)
      */
-    var name: String = "",
+    @SerializedName("name") var name: String = "",
     /**
      * id của quảng cáo
      */
-    var adUnitId: String = "",
+    @SerializedName("adUnitId") var adUnitId: String = "",
     /**
      * Có tự động tải lại quảng cáo khi đóng không dùng cho quảng cáo loại BaseShowAdGsData(app open, interstitial, rewarded, rewarded interstitial)
      */
-    var autoReloadWhenDismiss: Boolean = false,
+    @SerializedName("autoReloadWhenDismiss") var autoReloadWhenDismiss: Boolean = false,
     /**
      * Thời gian tải lại quảng cáo giữa các lần, đây là cấu hình mặc định ban đầu thôi, khi data đã thay đổi trong AdGsManager sẽ ko dùng nữa
      */
-    var delayTime: Long = 0L,
+    @SerializedName("delayTime") var delayTime: Long = 0L,
     /**
      * Dùng để xác định quảng cáo có được dùng không(thường dùng khi cấu hình trên firebase tắt bật)
      * isEnable = true tức là ứng dụng có sử dụng
      * isEnable = false tức là ứng dụng không sử dụng
      */
-    var isEnable: Boolean = true,
+    @SerializedName("isEnable") var isEnable: Boolean = true,
     /**
      * Loại quảng cáo được cấu hình ở AdGsType
      */
-    var adGsType: AdGsType = AdGsType.INTERSTITIAL
+    @SerializedName("adGsType") var adGsType: AdGsType = AdGsType.INTERSTITIAL
 ) {
 
     fun update(adPlaceName: AdPlaceName): AdPlaceName {
@@ -43,5 +46,9 @@ data class AdPlaceName(
         this.name = name
         this.adUnitId = adUnitId
         return this
+    }
+
+    fun isValidate(): Boolean {
+        return !TextUtils.isEmpty(adUnitId)
     }
 }
